73a5a8a8c0695b7110ae74b77501312334410a03
musta.akim <musta.akim@gmail.com>
2023-11-24 10:56:57 +0700

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,9 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs"; // for password hashing
 import { errorHandle } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
+// signup
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   // for password hashing
@@ -14,6 +16,33 @@ export const signup = async (req, res, next) => {
     // res.status(500).json(error.message);
     // from middleware
     // next(error);
-    next(errorHandle(500, "error coming from this midleware function"));
+    next(error);
+  }
+};
+
+// login
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email: email });
+    if (!validUser) return next(errorHandle(404, "User not found"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandle(401, "Wrong credentials"));
+    const token = jwt.sign(
+      {
+        id: validUser._id,
+      },
+      process.env.JWT_SECRET
+    );
+    const { password: pass, ...others } = validUser._doc;
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        //   expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+      })
+      .status(200)
+      .json(others);
+  } catch (error) {
+    next(error);
   }
 };
